30f6835befda25501c3a142e8612d2c7065a6029
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 13:05:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,31 @@ import { CookiePreferences } from "./components/CookiePreferences";
 import { HomePage } from "./pages/HomePage";
 import { ObituaryPage } from "./pages/ObituaryPage";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./sections/Navbar";
+import { Footer } from "./sections/Footer";
+import { FloatingButtons } from "./components/FloatingButtons";
+import { AccessibilityWidget } from "./components/AccessibilityWidget";
+import { CookiePreferences } from "./components/CookiePreferences";
+import { HomePage } from "./pages/HomePage";
+import { ObituaryPage } from "./pages/ObituaryPage";
+
 export const App = () => {
   return (
-    <div style={{ padding: 40, fontSize: 24 }}>
-      App is rendering ✅
-    </div>
+    <Router>
+      <div className="min-h-screen bg-[#fafafa]">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/obituaries/Rhonda-Mary-Olson" element={<ObituaryPage />} />
+        </Routes>
+
+        <Footer />
+        <FloatingButtons />
+        <AccessibilityWidget />
+        <CookiePreferences />
+      </div>
+    </Router>
   );
 };
