4001775612a35acbf43b3c5ed310fae0d1f4cccd
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 14:25:01 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
-  publicDir: "./static",
-  css: {
-    postcss: {
-      plugins: [tailwind()],
-    },
-  },
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
