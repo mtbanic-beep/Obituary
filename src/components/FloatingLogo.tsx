41bc7115aa6a38a0854de923617555a3aecfee67
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 16:53:31 -0500

    Create FloatingLogo.tsx

---
+++ src/components/FloatingLogo.tsx
@@ -0,0 +1,15 @@
+export const FloatingLogo = () => {
+  return (
+    <a
+      href="https://www.mhfh.com/"
+      aria-label="Go to McInnis & Holloway Funeral Homes home page"
+      className="fixed top-4 left-4 z-[9999]"
+    >
+      <img
+        src="https://c.animaapp.com/mj3qzcubLRevs0/assets/d490bcf7-231c-4a5e-85fc-22dd769b40da_thumbnail.webp"
+        alt="McInnis & Holloway Funeral Homes"
+        className="h-12 lg:h-16 object-contain"
+      />
+    </a>
+  );
+};
