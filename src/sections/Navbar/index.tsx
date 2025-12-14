49240afa74aa917e2a5dc35eb4a780c92e7d835f
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 12:01:43 -0500

    Update index.tsx

--- src/sections/Navbar/index.tsx
+++ src/sections/Navbar/index.tsx
@@ -1,5 +1,5 @@
-import { TopBar } from "./sections/Navbar/components/TopBar";
-import { MainNav } from "./sections/Navbar/components/MainNav";
+import { TopBar } from "./components/TopBar";
+import { MainNav } from "./components/MainNav";
 
 export const Navbar = () => {
   return (
