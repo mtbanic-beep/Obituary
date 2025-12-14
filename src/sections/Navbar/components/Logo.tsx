f55993a58dfdbe779c6a09bd5eed889ff7c92db5
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 16:29:00 -0500

    Update Logo.tsx

--- src/sections/Navbar/components/Logo.tsx
+++ src/sections/Navbar/components/Logo.tsx
@@ -1,22 +1,10 @@
 export const Logo = () => {
-  const goHome = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    window.location.assign("https://www.mhfh.com/");
-  };
-
   return (
-    <a
-      href="https://www.mhfh.com/"
-      onClick={goHome}
-      onMouseDown={(e) => e.stopPropagation()}
-      className="block flex-shrink-0 cursor-pointer relative z-[9999] pointer-events-auto"
-      aria-label="Go to McInnis & Holloway Funeral Homes home page"
-    >
+    <a href="https://www.mhfh.com/" className="block flex-shrink-0">
       <img
-        src="https://c.animaapp.com/mj3qzcubLRevs0/assets/d490bcf7-231c-4a5e-85fc-22dd769b40da_thumbnail.webp"
         alt="McInnis & Holloway Funeral Homes"
-        className="h-[80px] lg:h-[110px] xl:h-[135px] object-contain pointer-events-auto"
+        src="https://c.animaapp.com/mj3qzcubLRevs0/assets/d490bcf7-231c-4a5e-85fc-22dd769b40da_thumbnail.webp"
+        className="h-[80px] lg:h-[110px] xl:h-[135px] object-contain"
       />
     </a>
   );
