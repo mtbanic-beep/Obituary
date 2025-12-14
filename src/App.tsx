c008c3a569c2dff5e6ca356be41a498167120509
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 14:06:00 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./sections/Footer";
 import { FloatingButtons } from "./components/FloatingButtons";
 import { AccessibilityWidget } from "./components/AccessibilityWidget";
 import { CookiePreferences } from "./components/CookiePreferences";
-import { HomePage } from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import ObituaryPage from "./pages/ObituaryPage";
 
 const App = () => {
