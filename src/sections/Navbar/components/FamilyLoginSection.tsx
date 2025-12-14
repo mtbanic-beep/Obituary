435ede6e802ca210a1a764c119ef9ca307af9d41
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 12:12:32 -0500

    Update FamilyLoginSection.tsx

--- src/sections/Navbar/components/FamilyLoginSection.tsx
+++ src/sections/Navbar/components/FamilyLoginSection.tsx
@@ -1,4 +1,4 @@
-export const FamilyLoginSection = () => null;
+export const FamilyLoginSection = () => {
   return (
     <div className="bg-[#e5e5e5] text-center px-[45px] py-[30px] lg:py-12 font-lato">
       <div className="max-w-[1370px] w-full mx-auto px-[15px]">
@@ -7,10 +7,12 @@ export const FamilyLoginSection = () => null;
             Family Log In
           </h2>
         </div>
+
         <p className="text-[#3d3d3d] text-[15px] leading-[22.05px] mb-5 pb-[15px] font-source_sans_pro font-normal">
-          Authorized family members can securely access Rhonda's memorial website settings.
+          Authorized family members can securely access Rhonda&apos;s memorial website settings.
         </p>
-        <button className="text-white text-[15px] font-bold bg-[#f59e0b] leading-normal min-w-[136px] overflow-hidden px-[30px] py-[15px] rounded-[50px] hover:bg-[#d97706] transition-all shadow-[0_2px_5px_rgba(0,0,0,0.2)] hover:shadow-[0_4px_8px_rgba(0,0,0,0.3)] font-source_sans_pro uppercase tracking-[0.5px]">
+
+        <button className="text-white text-[15px] font-bold bg-[#59e0b] leading-normal min-w-[136px] overflow-hidden">
           Log In
         </button>
       </div>
