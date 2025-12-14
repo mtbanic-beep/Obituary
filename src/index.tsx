462886dabfebd29411eb31a96f2a58945dd6728f
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 13:00:18 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import "../tailwind.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById("app")!).render(
+// make sure your styles load (adjust if your file name differs)
+import "../tailwind.css"; // if tailwind.css is in project root
+// OR if tailwind.css is in src, use:  import "./tailwind.css";
+
+const el = document.getElementById("app");
+if (!el) throw new Error('Missing <div id="app"></div> in index.html');
+
+createRoot(el).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
