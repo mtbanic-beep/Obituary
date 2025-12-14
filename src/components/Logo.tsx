0916d8021ff0f3f3eb2bb28cf49600428e38bb66
mtbanic-beep <mtbanic@gmail.com>
2025-12-14 16:05:22 -0500

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,13 @@
 export const Logo = () => {
   return (
-    <a href="https://www.mhfh.com/" className="block">
+    <a
+      href="https://www.mhfh.com/"
+      className="block cursor-pointer transition hover:opacity-90"
+      aria-label="Go to McInnis & Holloway Funeral Homes home page"
+    >
       <img
+        src="https://c.animaapp.com/mj3qzcubLRevs0/assets/d490bcf7-231c-4a5e-85fc-22dd769b40da_thumbnail.webp"
         alt="McInnis & Holloway Funeral Homes"
-        src="https://c.animaapp.com/mj3qzcubLRevsO/assets/d490bcf7-231c-4a5e-85fc-22dd769b40da_thumbnail.webp"
         className="h-[130px] object-contain"
       />
     </a>
